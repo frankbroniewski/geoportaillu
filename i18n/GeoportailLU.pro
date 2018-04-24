0b4b1d24b6089829a420f0600ae4482d3986be76
Frank Broniewski <frank.broniewski@gmail.com>
2018-04-24 19:05:26 +0200

    German translation files

---
+++ i18n/GeoportailLU.pro
@@ -0,0 +1,3 @@
+FORMS = ..\geoportail_lu_dialog_base.ui
+SOURCES = ..\geoportail_lu.py
+TRANSLATIONS = GeoportailLU_de.ts
---
+++ i18n/GeoportailLU_de.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE" sourcelanguage="en_GB">
+<context>
+    <name>GeoportailLU</name>
+    <message>
+        <location filename="../geoportail_lu.py" line="195"/>
+        <source>&amp;Geoportal LU</source>
+        <translation>&amp;Geoportal LU</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu.py" line="181"/>
+        <source>Geoportal LU</source>
+        <translation>Geoportal LU</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu.py" line="287"/>
+        <source>Search Results</source>
+        <translation>Suchergebnisse</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu.py" line="295"/>
+        <source>Cannot load result layer</source>
+        <translation>Kann Ergebnislayer nicht laden</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu.py" line="296"/>
+        <source>Search Error</source>
+        <translation>Suchfehler</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu.py" line="299"/>
+        <source>Nothing do show!</source>
+        <translation>Nichts zum Zeigen!</translation>
+    </message>
+</context>
+<context>
+    <name>GeoportailLUDialogBase</name>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="14"/>
+        <source>Use the luxembourgish Geoportal in QGIS</source>
+        <translation>Das luxemburgische Geoportal in QGIS</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="33"/>
+        <source>Search</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="40"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="81"/>
+        <source>About</source>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="87"/>
+        <source>This Plugin allows you to search for locations in Luxembourg using the API from the luxembourgish Geoportal.
+https://geoportail.lu/</source>
+        <translation>Dieses Plugin erlaubt die Suche nachOrten mit Hilfe der API des luxemburgischen Geoportals</translation>
+    </message>
+    <message>
+        <location filename="../geoportail_lu_dialog_base.ui" line="125"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
+    </message>
+</context>
+</TS>
