c53bb41b5a284a5813197b1293630a92f21b5eeb
Frank Broniewski <hallo@frankbroniewski.com>
2020-03-28 09:22:21 +0100

    added geocode and reverse geocode functionality

--- services.py
+++ services.py
@@ -13,27 +13,41 @@ GEOCODE_ENDPOINT = "geocode/search"
 REVERSE_GEOCODE_ENDPOINT = "geocode/reverse"
 
 
-def search(query_string, limit=10):
-    """Search for an address or a land parcel by part of its name"""
-    url = "{}{}".format(API_URL, FIND_ENDPOINT)
-    r = requests.get(url, params={"query": query_string, "limit": limit})
-
+def do_request(url, params):
+    """perform a GET request to URL with the PARAMS"""
+    r = requests.get(url, params=params)
+    
     if r.status_code == requests.codes.ok:
         return r.json()
     else:
         r.raise_for_status()
 
 
-def geocode():
+def search(query_string, limit=10):
+    """Search for an address or a land parcel by part of its name"""
+    url = "{}{}".format(API_URL, FIND_ENDPOINT)
+    response = do_request(url, {"query": query_string, "limit": limit})
+    
+    return response
+
+
+def geocode(query_string):
     """Get coordinates for an address
-    TODO"""
-    pass
+    num, street, zip, locality"""
+    url = "{}{}".format(API_URL, GEOCODE_ENDPOINT)
+    response = do_request(url, {"queryString": query_string})
+    
+    return response
 
 
-def reverse_geocode():
+def reverse_geocode(coords):
     """Get address from coordinates
-    TODO"""
-    pass
+    coords = 'easting northing' """
+    coord = coords.split(' ')
+    url = "{}{}".format(API_URL, REVERSE_GEOCODE_ENDPOINT)
+    response = do_request(url, {"easting": coord[0], "northing": coord[1]})
+    
+    return response
 
 
 def main(args):
@@ -41,16 +55,28 @@ def main(args):
     if args.search:
         result = search(args.search)
         print(result)
+        
+    if args.geocode:
+        result = geocode(args.geocode)
+        print(result)
+    
+    if args.reverse:
+        result = reverse_geocode(args.reverse)
+        print(result)
 
 
 if __name__ == "__main__":
-    """Search for an address or a land parcel by part of its name
-    TODO implement CLI for geocoding"""
+    """Search for an address or a land parcel by part of its name"""
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--search",
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-s", "--search",
         help="Search for an address or a land parcel by part of its name")
+    group.add_argument("-g", "--geocode",
+        help="Geocode an address. Pattern number, street, zip, locality")
+    group.add_argument("-r", "--reverse", 
+        help="Reverse geocode a location to an address")
     args = parser.parse_args()
 
     main(args)
