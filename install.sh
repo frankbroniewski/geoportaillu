f47f0607794a5ea46599b9e9c391cbdbe85f091d
Frank Broniewski <hallo@frankbroniewski.com>
2020-05-20 09:15:44 +0200

    install plugin to QGIS plugin folder (development only)

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# copy the plugin files to the QGIS plugin folder of the current user
+# delete the existing plugin beforehand
+
+PLUGIN_DIR="$HOME/.local/share/QGIS/QGIS3/profiles/default/python/plugins/geoportail_lu/"
+SRC_DIR="$PWD"
+
+# remove the old stuff
+rm -rf "$PLUGIN_DIR"
+
+mkdir -p "$PLUGIN_DIR"
+# copy over the new things
+cp -a "$SRC_DIR/"*.py "$SRC_DIR/"README* "$SRC_DIR/"*.txt \
+      "$SRC_DIR/"*.ui "$SRC_DIR/"resources.qrc "$SRC_DIR/"icon.png \
+      "$SRC_DIR/"help "$SRC_DIR/"i18n "$SRC_DIR/"scripts \
+      "$PLUGIN_DIR"
